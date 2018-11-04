2a1519b304f28a67c7bb54ad51b4d9d288d0b9c7
Mikael Kalms <mikael@kalms.org>
2018-11-04 14:24:02 +0100

    Proof of concept, logs beginsession/endsession

---
+++ Assets/LogSession.cs
@@ -0,0 +1,10 @@
+﻿public static class LogSession
+{
+    public static EventLogger.Session session;
+
+    public static void Log(string type, object data)
+    {
+        if (session != null)
+            session.SubmitEvent(type, data);
+    }
+}
---
+++ Assets/LogSessionInitializer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LogSessionInitializer : MonoBehaviour {
+
+    private EventLogger.EventTransmitter transmitter;
+    private EventLogger.Session session;
+
+    public bool LogToBackend = true;
+    public EventLogger.BackendTransmitter.Configuration BackendTransmitterConfig;
+    public bool LogToDisk = true;
+    public EventLogger.DiskLogger.Configuration DiskLoggerConfig;
+
+    void Awake()
+    {
+        transmitter = new EventLogger.EventTransmitter(LogToBackend ? BackendTransmitterConfig : null, LogToDisk ? DiskLoggerConfig : null);
+
+        string sessionId = EventLogger.Session.GetNewSessionId();
+
+        session = new EventLogger.Session(sessionId, transmitter);
+        session.Begin();
+
+        LogSession.session = session;
+    }
+
+    void Update()
+    {
+        transmitter.Update(Time.deltaTime);
+    }
+
+    void OnDestroy()
+    {
+        LogSession.session = null;
+
+        session.End();
+        transmitter.Flush();
+    }
+}
